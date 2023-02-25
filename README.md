90c2e3bff783e9bab614f8db915dafe5cad552ff
G Adam Cox <gadamc@gmail.com>
2023-02-24 16:00:38 -0800

    adds messy code on purpose in case need to go back to what I was thinking previously. cleaning up in next commit

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ for multiple coupled spins in a transverse uniform magnetic field using Pennylan
 We plan to test and study the fidelity (our cost function) at each time step, as well as Pauli spin 
 observables and compare our results to the published outcomes. 
 
+#### Notes
+
+Pytorch does not yet support Python 3.11. Using Python 3.10.9.
+
+After installing pennylane, I am trying a number of different ML platforms because autograd didn't work
+initially. I first installed JAX, but was getting errors. I then installed PyTorch into the same environment. 
+However, this installation order caused an error:
+
+```
+ImportError: cannot import name 'COMMON_SAFE_ASCII_CHARACTERS' from 'charset_normalizer.constant'
+```
+
+Previously, I installed pennylane, then pytorch and did not receive this error. So, once we decide upon the 
+correct ML tool, we probably won't run into this problem. But, if you are following along and installed 
+pennylane, then JAX, then PyTorch, to work around this error I then installed chardet `pip install chardet` and
+the import error above goes away. 
+
--- circuit.py
+++ circuit.py
@@ -1,12 +1,9 @@
+import sys
 import pennylane as qml
-import pennylane.numpy as np
-
-dev = qml.device("default.qubit", wires=range(3))
-
 
 def hamiltonian(coupling_strength=0.25, field_strength=1):
-    coeffs = [-field_strength]*3
-    coeffs += [coupling_strength]*6
+    coefficients = [-field_strength]*3
+    coefficients += [coupling_strength]*6
 
     obs = [qml.PauliX(0), qml.PauliX(1), qml.PauliX(2),
            qml.PauliX(0) @ qml.PauliX(1),
@@ -16,24 +13,26 @@ def hamiltonian(coupling_strength=0.25, field_strength=1):
            qml.PauliY(1) @ qml.PauliY(2),
            qml.PauliZ(1) @ qml.PauliZ(2),
            ]
-    return qml.Hamiltonian(coeffs, obs)
+    return qml.Hamiltonian(coefficients, obs)
 
 
-def three_spins_circuit(params):
+def three_spins_circuit(params, n_spins = 3, depth= 3):
     """
     Constructs the parameterized circuit that transforms the |000> state to our time-evolved state.
 
-    NB -- this is an explict declaration of the circuit rather for n = 3 spin components and a circuit
+    NB -- this is an explict declaration of the circuit for n = 3 spin components and a circuit
     depth of 3. Future versions of this code could allow for arbitrary values
     :param params: array-like object of size 18; (depth+1)*n + depth*(n-1)
-    :return:
+    :param n_spins: number of particles with spin state
+    :param depth: depth of the circuit
+    :return: None
     """
     qml.RX(params[0], wires=0)
     qml.RX(params[1], wires=1)
     qml.RX(params[2], wires=2)
 
-    qml.IsingZZ(params[3], wires=[0,1])
-    qml.IsingZZ(params[4], wires=[1,2])
+    qml.IsingZZ(params[3], wires=[0, 1])
+    qml.IsingZZ(params[4], wires=[1, 2])
 
     qml.RY(params[5], wires=0)
     qml.RY(params[6], wires=1)
@@ -53,14 +52,119 @@ def three_spins_circuit(params):
     qml.RY(params[16], wires=1)
     qml.RY(params[17], wires=2)
 
+def run(interface):
 
-@qml.qnode(dev)
-def three_spins(time, n):
-    """
+    if interface == 'autograd':
+        import pennylane.numpy as np
+        dev = qml.device("default.qubit", wires=range(3))
+        params = 0.05 * np.ones(18)
 
-    """
+    elif interface == 'jax':
+        import jax
+        import jax.numpy as jnp
+        # next line raises TypeError with default.qubit.jax
+        # dev = qml.device("default.qubit.jax", wires=range(3))
+        dev = qml.device("default.qubit", wires=range(3))
+        params = jnp.array([0.05] * 18)
+    elif interface == 'torch':
+        import torch
+        dev = qml.device("default.qubit", wires=range(3))
+        params = torch.tensor([0.05] * 18, requires_grad=True)
+    else:
+        return -1
 
-    params = np.random.random(18)
-    three_spins_circuit(params)
-    qml.ApproxTimeEvolution(hamiltonian(), time, n)
-    return qml.state()
+    @qml.qnode(dev, interface=interface)
+    def three_spins_forward(params, delta_time, n=1):
+        """
+        params - array / tensor of size 18
+
+        """
+        three_spins_circuit(params)
+        # print(pre_time_evolution_state)
+
+        # or something like --> qml.exp(hamiltonian(), delta_time)?
+        # all I really want to do is U(delta_time) = exp(-i * hamiltonian() *delta_time)
+        # That is... I only want to take one trotter step for this small delta_time.
+        # Does ApproxTimeEvolution(hamiltonion(), delta_time, n = 1) accomplish that?
+        # Although...it can't hurt to have a n > 1, as we still are only performing U(deltat_time)
+        qml.ApproxTimeEvolution(hamiltonian(), delta_time, n)
+        #print(qml.state())
+
+        # Seems like we can't use this for computing gradients -- raises exceptions
+        #fidelity = qml.math.fidelity(pre_time_evolution_state, qml.state())
+        # return fidelity
+        # looking at this --> https://strawberryfields.ai/photonics/demos/run_state_learner.html
+
+        # this works --
+        # return qml.expval(qml.PauliZ(0))
+
+        # this does not work -- error is RX must occur prior to measurement
+        # Obviously I cannot read the quantum state, then perform an operation!
+        # pre_time_evolution_state = qml.state()
+        # qml.RX(params[0], wires=0)
+        # return [pre_time_evolution_state, qml.state()]
+        #
+        #  same error here
+        # qml.RX(params[0], wires=0)
+        # return qml.state()
+
+        return qml.state()
+
+    @qml.qnode(dev, interface=interface)
+    def three_spins_current(params):
+        three_spins_circuit(params)
+        return qml.state()
+    dt = 0.1
+    original_state = three_spins_current(params)
+    forward_state = three_spins_forward(params + 0.01, dt)
+
+    fidelity = qml.math.fidelity(original_state, forward_state)
+
+    return fidelity
+
+if __name__ == '__main__':
+    print(sys.argv)
+    print(run(sys.argv[1]))
+
+# @qml.qnode(dev, interface=interface)
+# def three_spins(params, delta_time, n=1):
+#     """
+#     params - array / tensor of size 18
+#
+#     """
+#     three_spins_circuit(params)
+#     #pre_time_evolution_state = qml.state()
+#     #print(pre_time_evolution_state)
+#     qml.ApproxTimeEvolution(hamiltonian(), delta_time, n)  # or should we just do qml.exp(hamiltonian(), delta_time)?
+#     #print(qml.state())
+#     # fidelity = qml.math.fidelity(pre_time_evolution_state, qml.state())
+#     return qml.expval(qml.PauliZ(0))
+
+# if __name__ == '__main__':
+#     if interface == 'jax':
+#         params = jnp.array(0.05 * [1]*18)
+#     elif interface == 'autograd':
+#         params = 0.05 * np.ones(18)
+#
+#     cost_threshold = 0.9999
+#     max_iterations = 50
+#
+#     delta_time = 0.1
+#     total_time = 2.0
+#
+#     for time_step_index in range(1, int(total_time / delta_time)):
+#         current_time = time_step_index * delta_time
+#         fidelity = 0
+#         num_iterations = 0
+#
+#         while fidelity < cost_threshold and num_iterations < max_iterations:
+#             fidelity = three_spins(params, delta_time)
+#
+#
+#
+#             cost = (1 - fidelity)   # / delta_time**2
+#             num_iterations += 1
+
+
+
+
